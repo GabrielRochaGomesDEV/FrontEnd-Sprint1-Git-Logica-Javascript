ef2089fb5f26d588386dcbf93682105168443591
Gabriel Rocha <gabrielrocha28@gmail.com>
2024-10-03 08:06:36 -0300

    adicionado exe3.js

---
+++ Lista 3 - Estrutura Sequencial/Resolucao/exe3.js
@@ -0,0 +1,11 @@
+// tempo de duração de um vento
+
+var tempoSegundos = 3600;
+
+    console.log(tempoSegundos);
+
+var tempoMinutos = tempoSegundos / 60;
+    console.log(tempoMinutos); 
+    
+var tempoHoras = tempoMinutos / 60;
+console.log(tempoHoras);    
