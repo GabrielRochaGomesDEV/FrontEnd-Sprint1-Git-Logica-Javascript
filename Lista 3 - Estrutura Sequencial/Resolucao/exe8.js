6497c787a58a482baebb76ba82f300d29b65fbca
Gabriel Rocha <gabrielrocha28@gmail.com>
2024-10-03 08:53:37 -0300

    Adicionado exe8.js

---
+++ Lista 3 - Estrutura Sequencial/Resolucao/exe8.js
@@ -0,0 +1,13 @@
+var custoDeFabrica = 10000;
+
+     // Custo do distribuidor = 30% do custo de fabrica
+
+var custoDistribuidor = custoDeFabrica * 0.3;
+
+     // Custo dos impostos = 45% do custo de fabrica
+
+var custoImpostos = custoDeFabrica * 0.45;
+
+     // Custo do consumidor = soma de todos os custos
+
+var custoConsumidor = custoDeFabrica + custoDistribuidor + custoImpostos;
