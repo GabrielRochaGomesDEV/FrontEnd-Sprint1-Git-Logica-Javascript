00982e4a17c5823c29275de54517f9018b147d31
Gabriel Rocha <gabrielrocha28@gmail.com>
2024-10-07 15:14:00 -0300

    Adicionado exe4.js lista 4

---
+++ Lista 4 - Estrutura Condicional e de Repetição/Resolução/exe4.js
@@ -0,0 +1,17 @@
+var numero;
+
+numero = -3;
+
+if(numero % 2 == 0) {
+    console.log("O número é par!")
+}
+else {
+    console.log("O número é ímpar!")
+}
+
+if(numero < 0 ) {
+    console.log("O número é negativo!");
+}
+else {
+    console.log("O número é positivo!");
+}
