b1d1d9bc627a6b1c6f06a6a942c49b5b1a0f245c
Gabriel Rocha <gabrielrocha28@gmail.com>
2024-10-07 15:37:26 -0300

    Adicionado exe7.js lista 4

---
+++ Lista 4 - Estrutura Condicional e de Repetição/Resolução/exe7.js
@@ -0,0 +1,7 @@
+// Altero a estrutura do for para que ele percorra do 10 até o 0
+// LOOP para exibir numeros de 10 a 1 no console
+
+
+for (let index = 10; index > 0; index--) {
+    console.log(index + "\n")    
+}
