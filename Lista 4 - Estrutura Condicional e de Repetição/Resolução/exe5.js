2421d87ce42371af679edfabdc8eead93d246f96
Gabriel Rocha <gabrielrocha28@gmail.com>
2024-10-07 15:22:10 -0300

    Adicionado exe5.js lista 4

---
+++ Lista 4 - Estrutura Condicional e de Repetição/Resolução/exe5.js
@@ -0,0 +1,14 @@
+var numero1, numero2;
+
+numero1 = 44;
+numero2 = 25;
+
+if(numero1 > numero2) {
+    console.log("O primeiro é maior!")
+}
+else if(numero2 > numero1) {
+    console.log("O segundo é maior!");
+}
+else {
+    console.log("Os números são iguais!");
+}
