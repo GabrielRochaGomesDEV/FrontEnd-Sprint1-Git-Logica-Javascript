53a11763db577ef16bbf7290b0975ba043785b45
Gabriel Rocha <gabrielrocha28@gmail.com>
2024-10-09 15:17:50 -0300

    Adiconado exe12.js lista 4

---
+++ Lista 4 - Estrutura Condicional e de Repetição/Resolução/exe12.js
@@ -0,0 +1,23 @@
+var numero = 4;
+
+if(numero == 1) {
+    console.log("1 - Domingo");
+}
+else if(numero == 2) {
+    console.log("2 - Segunda-Feira");
+}
+else if(numero == 3) {
+    console.log("3 - Terça-Feira");
+}
+else if(numero == 4) {
+    console.log("4 - Quarta-Feira");
+}
+else if(numero == 5) {
+    console.log("5 - Quinta-Feira");
+}
+else if(numero == 6) {
+    console.log("6 - Sexta-Feira");
+}
+else {
+    console.log("7 - Sábado");
+}
