db112b37f259dfc9ca3c6a1e337d34d7827516cc
Gabriel Rocha <gabrielrocha28@gmail.com>
2024-10-09 14:54:03 -0300

    Adicionado exe10.js lista 4

---
+++ Lista 4 - Estrutura Condicional e de Repetição/Resolução/exe10.js
@@ -0,0 +1,12 @@
+var N;
+
+N = 11;
+
+// Basta percorrer todos os números entre 0 e N exibindo aqueles que são pares
+
+for (let index = 0; index <= N; index++) {
+    if(index % 2 == 0) {
+        console.log(index);
+    }
+    
+}
