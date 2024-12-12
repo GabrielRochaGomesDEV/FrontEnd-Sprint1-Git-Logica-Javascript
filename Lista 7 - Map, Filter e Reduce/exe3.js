d5ea305f5ceca802708ae5049cc3f9604979e4db
Gabriel Rocha <163765743+GabrielRochaGomesDEV@users.noreply.github.com>
2024-12-12 13:04:24 -0300

    Adicionado exe3.js lista 7

---
+++ Lista 7 - Map, Filter e Reduce/exe3.js
@@ -0,0 +1,8 @@
+const A = [3, 4, 8, 9, 15];
+
+// Utilizar o map para calcular o cubo de cada elemento
+const cubos = A.map(num => num ** 3);
+
+console.log(cubos); 
+
+// Saída: [27, 64, 512, 729, 3375]
